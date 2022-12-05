f17984a1d60c9b47b1feaa1a41150f78d035a3be
YAKSH KOTHARI <kothari.yaksh.19bsd019@gmail.com>
2022-12-06 01:12:36 +0530

    JavaScript Changes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert('Damn! This Thing Works, WOAHHH!!!')
+alert("Damn! This Thing Works, WOAHHH!!!");
+
+hide = () => {
+  document.getElementById("firstCointainer").style.display = "none";
+};
+
+show = () =>{
+  document.getElementById("firstCointainer").style.display = "block";
+};
+
+colorChange = () =>{
+  document.getElementById("secondcontainers").style.background ="rgb(134, 82, 153)";
+}; 
+
